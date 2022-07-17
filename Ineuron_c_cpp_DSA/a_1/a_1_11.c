55f4766301a8123f2313418264d6645fd3fdd5c5
tarun-reddy-13 <96988906+tarun-reddy-13@users.noreply.github.com>
2022-07-17 10:05:55 +0530

    Add files via upload

---
+++ Ineuron_c_cpp_DSA/a_1/a_1_11.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int HH,MM;
+	printf("Enter time in the format (HH:MM) :");
+	scanf("%d%d",&HH,&MM);
+	printf("%d hour and %d Minute",HH,MM);
+	return 0;
+}
---
+++ Ineuron_c_cpp_DSA/a_1/a_1_5.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	int x;
+	x=printf("Length of this string is : ");
+	printf("%d",x);
+	return 0;
+}
